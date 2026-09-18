0f5cbf8c7eb4b2dd3b3855ba89d336234eaa389b
gizmore <gizmore@wechall.net>
2026-09-18 04:06:55 +0200

    'more korean'

---
+++ lang/guestbook_ko.php
@@ -0,0 +1,98 @@
+<?php
+return [
+	'link_guestbook' => '방명록',
+	'gdo_guestbook' => '방명록',
+	'gbm_guestbook' => '방명록',
+	'link_your_guestbook' => '귀하의 방명록',
+	'gdo_guestbookmessage' => '방명록 입력',
+	'cfg_gb_ipp' => '페이지당 메시지',
+	'cfg_gb_allow_url' => '각 항목에 대해 웹사이트 링크를 허용하시겠습니까?',
+	'cfg_gb_allow_email' => '항목에 대한 이메일을 공개하도록 허용하시겠습니까?',
+	'cfg_gb_allow_guest_view' => '손님이 방명록을 볼 수 있도록 허용하시겠습니까?',
+	'cfg_gb_allow_guest_sign' => '손님이 방명록에 서명하도록 허용하시겠습니까?',
+	'cfg_gb_allow_level' => '서명을 위한 최소 수준 설정을 허용하시겠습니까?',
+	'cfg_gb_allow_user_gb' => '사용자가 방명록을 만들 수 있도록 허용하시겠습니까?',
+	'cfg_gb_user_gb_level' => '자신의 방명록에 필요한 사용자 수준',
+	'cfg_gb_guest_captcha' => '손님에게 보안문자를 강제 적용하시겠습니까?',
+	'cfg_gb_member_captcha' => '회원에게 보안문자를 강제 적용하시겠습니까?',
+	'cfg_gb_left_bar' => '왼쪽 사이드바에 사이트 방명록을 표시하시겠습니까?',
+	'cfg_gb_right_bar' => '오른쪽 사이드바에 사용자 방명록을 표시하시겠습니까?',
+	'link_create_guestbook' => '방명록 만들기',
+	'link_edit_guestbook' => '방명록 편집',
+	'cfg_user_guestbook' => '귀하의 방명록 ID',
+	'guestbook_level' => '보고 서명할 수 있는 최소 수준입니다.',
+	'list_view_guestbook' => '방명록에 %s개의 항목이 있습니다.',
+	'link_sign_guestbook' => '방명록에 서명하세요',
+	'link_gb_approval_list' => '보통 항목',
+	'view_users_guestbook' => '%s의 방명록',
+	'err_no_guestbook' => '방명록이 존재하지 않습니다.',
+	'list_guestbook_view' => '%s개의 방명록 항목입니다.',
+	'div_gb_appearance' => '외관',
+	'guestbook_default_title' => '방명록',
+	'guestbook_default_descr' => '%s의 방명록',
+	'div_gb_signing' => '서명',
+	'gb_unlocked' => '서명이 활성화되어 있습니까?',
+	'gb_moderated' => '항목을 검토해야 합니까?',
+	'gb_notify_mail' => '이메일 알림을 활성화하시겠습니까?',
+	'div_gb_permissions' => '권한',
+	'gb_guest_view' => '손님이 방명록을 볼 수 있도록 허용하시겠습니까?',
+	'gb_guest_sign' => '손님이 방명록에 서명하도록 허용하시겠습니까?',
+	'div_gb_metadata' => '메타데이터',
+	'gb_allow_email' => '사용자가 이메일을 게시하도록 허용하시겠습니까?',
+	'gb_allow_url' => '사용자가 웹사이트를 게시하도록 허용하시겠습니까?',
+	'mt_guestbook_sign' => '방명록에 서명하세요',
+	'gbm_email_public' => '이메일을 대중에게 표시하시겠습니까?',
+	'err_guestbook_sign' => '방명록에 서명하실 수 없습니다.',
+	'err_guestbook_locked' => '방명록은 현재 메시지 작성이 비활성화되어 있습니다.',
+	'err_no_guests' => '손님은 방명록에 서명할 수 없습니다.',
+	'msg_gb_moderation' => '귀하가 생성한 항목이 표시되기 전에 승인을 받아야 합니다.',
+	'msg_gb_signed' => '방명록을 작성해주셔서 감사합니다. 이제 표시됩니다.',
+	'list_guestbook_approvelist' => '승인할 항목 %s개',
+	'err_already_approved' => '이 방명록 항목은 이미 승인되었습니다.',
+	'msg_gbm_approved' => '방명록 메시지가 표시되었습니다.',
+	'err_gbmsg_not_found' => '방명록 항목을 찾을 수 없습니다.',
+	'msg_gbmsg_deleted' => '방명록 항목이 삭제된 것으로 표시되었습니다.',
+	'link_approvals' => '승인 대기 중',
+	'link_site_guestbook' => '사이트 방명록 구성',
+	'link_configure_gb_module' => '모듈 구성',
+	'gbm_approved' => '승인 시간',
+	'mail_subj_gb_moderate' => '%s 방명록 항목',
+	'mail_body_gb_moderate' => '안녕하세요 %s님,
+    
+%s에 방명록 항목이 작성되었습니다.
+    
+이메일: %s
+웹사이트: %s
+메시지:
+================================
+%s
+================================
+한 번의 클릭으로 이 메시지를 승인할 수 있습니다.
+%s
+
+한 번의 클릭으로 이 메시지를 삭제할 수 있습니다.
+%s
+================================
+    
+감사합니다,
+%2$s 팀',
+	'mail_subj_notify_gb' => '%s 방명록 항목',
+	'mail_body_notify_gb' => '안녕하세요 %s님,
+
+%s에 방명록 항목이 작성되었습니다.
+
+이메일: %s
+웹사이트: %s
+메시지:
+================================
+%s
+================================
+이 항목은 자동으로 승인되었습니다.
+
+한 번의 클릭으로 이 메시지를 삭제할 수 있습니다.
+%s
+================================
+
+감사합니다,
+%2$s 팀',
+];
